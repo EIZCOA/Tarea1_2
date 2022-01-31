b8ecbe1d380f2b546520d84ea0b499d95c2ceeea
Fausto Iscoa <emiliscoa@outlook.com>
2022-01-31 15:30:11 -0600

    Initial commit

---
+++ Tarea1_2/Tarea1_2/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            //Declarar nuestro Binding context
+
+            var Contact = new Models.Contactos
+            {
+                nombre = edtnombre.Text.Trim(),
+                apellido = edtapellido.Text.Trim(),
+                edad =  Convert.ToInt32(edtedad.Text.Trim()),
+                correo = edtemail.Text.Trim()
+
+            };
+
+            //cp: Content Page
+            var cpContacto = new Contacto();
+
+            cpContacto.BindingContext = Contact;
+
+            await Navigation.PushModalAsync(cpContacto);
+        }
+    }
+}
---
+++ Tarea1_2/Tarea1_2/Models/Contactos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea1_2.Models
+{
+    public class Contactos
+    {
+       public String nombre { get; set; }
+       public String apellido { get; set; }
+       public Int32 edad { get; set; }
+       public String correo { get; set; }
+
+    }
+}
